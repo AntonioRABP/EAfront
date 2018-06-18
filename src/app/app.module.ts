dc451141f38cc6de2545857854aba3b65cb2a636
antonio <bacaperez.a@gmail.com>
2018-06-18 00:34:24 -0500

    agregar funcionalidad de crear usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,13 @@ import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
-<<<<<<< HEAD
+
 import { ExamenAlumnoPage } from '../pages/examen-alumno/examen-alumno'
-=======
-import { ExamenAlumnoPage } from '../pages/examen-alumno/examen-alumno';
+
 import { CrearExamenPage } from '../pages/crear-examen/crear-examen';
 import { CrearPreguntaPage } from '../pages/crear-pregunta/crear-pregunta';
 import { PerfilProfesorPage } from '../pages/perfil-profesor/perfil-profesor';
->>>>>>> 564cc7a4786dab6f87d4d230676515a1ad4ba87a
+
 import { TimerPage } from '../pages/timer/timer'
 
 
