da932346de36ef6a968d7f264ff8496cd0036af2
da_flores95 <dflorest@uni.edu.pe>
2018-06-19 00:38:09 -0700

    Registro_Definitivo
    La vista del registro, añadi funcionalidad a los botones

--- src/pages/perfil-estudiante/perfil-estudiante.ts
+++ src/pages/perfil-estudiante/perfil-estudiante.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MenuController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the PerfilEstudiantePage page.
@@ -15,10 +16,11 @@ import { MenuController } from 'ionic-angular';
   templateUrl: 'perfil-estudiante.html',
 })
 export class PerfilEstudiantePage {
-
+  noActualizar: boolean = true;
   constructor(public navCtrl: NavController, 
   			  public navParams: NavParams,
-  			  public menuCtrl: MenuController) {
+  			  public menuCtrl: MenuController,
+          public alerCtrl: AlertController) {
 	 this.menuCtrl.enable(true,'MenuStudent');
   	this.menuCtrl.enable(false,'MenuTeacher');
   }
@@ -27,4 +29,33 @@ export class PerfilEstudiantePage {
     console.log('ionViewDidLoad PerfilEstudiantePage');
   }
 
+  actualizarData(){
+    this.noActualizar = false;
+  }
+
+  atrasData(){
+    this.noActualizar = true;
+  }
+
+  doConfirm() {
+    let confirm = this.alerCtrl.create({
+      title: 'Actualizar perfil?',
+      message: 'Esta usted seguro de actualizar los datos de su perfil?',
+      buttons: [
+        {
+          text: 'Rechazar',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Confirmar',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present()
+}
 }
