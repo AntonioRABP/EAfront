23c81af17a3ba1691566a256fba8e0cee12a7ca4
da_flores95 <dflorest@uni.edu.pe>
2018-06-21 22:32:46 -0700

    Menu Estudiante funciona
    Ya funciona el menu del estudiante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Nav } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
@@ -17,10 +18,13 @@ import { PerfilEstudiantePage } from '../pages/perfil-estudiante/perfil-estudian
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
 
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, 
+              statusBar: StatusBar, 
+              splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -29,5 +33,12 @@ export class MyApp {
     });
   }
 
+  openPagePerfil(){
+    this.nav.setRoot(PerfilEstudiantePage)
+  }
+
+  openPageExEstudiante(){
+    this.nav.setRoot(ExamenAlumnoPage)
+  }
 }
 
